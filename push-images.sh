f8f8d45535d3d0178e264d89b26bb5c5d2d2725a
brunneis <dev@brunneis.com>
2018-11-09 09:48:23 +0100

    Add push-images.sh script

---
+++ push-images.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+docker push catenae/kafka
+docker push catenae/erdd-aerospike-storer
+docker push catenae/rut-links
+docker push catenae/erdd-links
+docker push catenae/web-app
+docker push catenae/stats-link
